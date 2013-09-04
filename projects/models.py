ad8f3c5c97c331fb55bb5ffddb078f59f517c161
nvbn <nvbn.rm@gmail.com>
2013-09-04 13:58:29 +0400

    Add receiving repos from github

--- projects/models.py
+++ projects/models.py
@@ -1,6 +1,30 @@
+from github import Github
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
+
+
+class ProjectManager(models.Manager):
+    """Project manager"""
+
+    def _get_remote_projects(self, user):
+        """Get remote projects"""
+        token = user.social_auth.get().extra_data['access_token']
+        github = Github(token)
+        github_user = github.get_user()
+        return github_user.get_repos('public')
+
+    def get_or_create_for_user(self, user):
+        """Get or create for user"""
+        for repo in self._get_remote_projects(user):
+            # TODO: add organization support
+            if not repo.organization:
+                Project.objects.get_or_create(
+                    owner=user,
+                    name=repo.name,
+                    url=repo.url,
+                )
+        return self.filter(owner=user)
 
 
 class Project(models.Model):
@@ -11,6 +35,8 @@ class Project(models.Model):
     is_enabled = models.BooleanField(
         default=False, verbose_name=_('is enabled'),
     )
+
+    objects = ProjectManager()
 
     class Meta:
         verbose_name = _('Project')
--- projects/tests/test_models.py
+++ projects/tests/test_models.py
@@ -1,4 +1,7 @@
+from mock import MagicMock
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .. import models
 from . import factories
 
 
@@ -16,3 +19,45 @@ class RelationsCase(TestCase):
         branch = factories.BranchFactory()
         factories.CommitFactory.create_batch(10, branch=branch)
         self.assertEqual(branch.commits.count(), 10)
+
+
+class ProjectManagerCase(TestCase):
+    """Project manager case"""
+
+    def setUp(self):
+        self._mock_github_call()
+        self.user = User.objects.create_user('user')
+
+    def _mock_github_call(self):
+        """Mock github call"""
+        self._orig_get_remote_projects =\
+            models.ProjectManager._get_remote_projects
+        models.ProjectManager._get_remote_projects = MagicMock()
+
+    def tearDown(self):
+        models.ProjectManager._get_remote_projects =\
+            self._orig_get_remote_projects
+
+    def _create_repo(self, n):
+        """Create repo"""
+        repo = MagicMock(
+            url='http://test{}.com'.format(n),
+            organization=None,
+        )
+        repo.name = 'project {}'.format(n)
+        return repo
+
+    def test_create(self):
+        """Test create"""
+        models.ProjectManager._get_remote_projects.return_value =\
+            map(self._create_repo, range(10))
+        projects = models.Project.objects.get_or_create_for_user(self.user)
+        self.assertEqual(projects.count(), 10)
+
+    def test_get(self):
+        """Test get without creating new"""
+        factories.ProjectFactory.create_batch(10, owner=self.user)
+        models.ProjectManager._get_remote_projects.return_value =\
+            map(self._create_repo, range(1, 11))
+        projects = models.Project.objects.get_or_create_for_user(self.user)
+        self.assertEqual(projects.count(), 10)
