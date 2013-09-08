f6071285cd421634a448788ea4d3d54d20dec359
nvbn <nvbn.rm@gmail.com>
2013-09-08 20:44:23 +0400

    Fix pep8 violations count

--- violations/pep8.py
+++ violations/pep8.py
@@ -6,8 +6,8 @@ from .base import library
 @library.register('pep8')
 def pep8_violation(data):
     """PEP8 violation parser"""
-    count = len(data['raw'].split('\n'))
-    data['status'] = STATUS_SUCCESS if count <= 1 else STATUS_FAILED
+    count = len(data['raw'].split('\n')) - 1
+    data['status'] = STATUS_SUCCESS if count == 0 else STATUS_FAILED
     data['preview'] = render_to_string('violations/pep8/preview.html', {
         'count': count,
     })
