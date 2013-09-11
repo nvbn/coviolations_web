bb62e75254714b035ade39289fbd52720265b676
nvbn <nvbn.rm@gmail.com>
2013-09-11 09:07:17 +0400

    Add pip-review violation case

--- violations/tests/test_pip_review.py
+++ violations/tests/test_pip_review.py
@@ -1,24 +1,24 @@
 from django.test import TestCase
 from tasks.const import STATUS_SUCCESS, STATUS_FAILED
-from ..pep8 import pep8_violation
+from ..pip_review import pip_review_violation
 from .base import get_content
 
 
-class PEP8ViolationCase(TestCase):
-    """PEP8 violation case"""
+class PipReviewViolationCase(TestCase):
+    """Pip review violation case"""
 
     def test_success(self):
         """Test success result"""
         data = {'raw': ''}
-        result = pep8_violation(data)
+        result = pip_review_violation(data)
         self.assertEqual(result['status'], STATUS_SUCCESS)
-        self.assertEqual(result['plot']['count'], 0)
+        self.assertEqual(result['plot']['outdated'], 0)
 
     def test_fail_on_real(self):
         """Test fail on real data"""
         data = {
-            'raw': get_content('pep8.out'),
+            'raw': get_content('pip_review.out'),
         }
-        result = pep8_violation(data)
+        result = pip_review_violation(data)
         self.assertEqual(result['status'], STATUS_FAILED)
-        self.assertEqual(result['plot']['count'], 307)
+        self.assertEqual(result['plot']['outdated'], 12)
