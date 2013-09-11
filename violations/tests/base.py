afddb963ed74137a684e6467557e9961be047fef
nvbn <nvbn.rm@gmail.com>
2013-09-11 09:10:35 +0400

    Move violations fixtures to special dir

--- violations/tests/base.py
+++ violations/tests/base.py
@@ -4,5 +4,5 @@ import os
 def get_content(file_name):
     """Get content of near file"""
     return open(os.path.abspath(
-        os.path.join(os.path.dirname(__file__), file_name),
+        os.path.join(os.path.dirname(__file__), 'fixtures', file_name),
     )).read()
