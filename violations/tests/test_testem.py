f35105941c8d752ab287c940c32dc4dee7bc00cd
nvbn <nvbn.rm@gmail.com>
2013-09-11 09:21:50 +0400

    Add testem violation test case

---
+++ violations/tests/test_testem.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+from tasks.const import STATUS_SUCCESS, STATUS_FAILED
+from ..testem import testem_violation
+from .base import get_content
+
+
+class TestemViolationCase(TestCase):
+    """Testem violation case"""
+
+    def test_success(self):
+        """Test success result"""
+        data = {
+            'raw': get_content('testem_success.out'),
+        }
+        result = testem_violation(data)
+        self.assertEqual(result['status'], STATUS_SUCCESS)
+        self.assertEqual(result['plot']['tests'], 34)
+        self.assertEqual(result['plot']['pass'], 34)
+        self.assertEqual(result['plot']['fail'], 0)
+
+    def test_fail(self):
+        """Test fail result"""
+        data = {
+            'raw': get_content('testem_fail.out'),
+        }
+        result = testem_violation(data)
+        self.assertEqual(result['status'], STATUS_FAILED)
+        self.assertEqual(result['plot']['tests'], 35)
+        self.assertEqual(result['plot']['pass'], 34)
+        self.assertEqual(result['plot']['fail'], 1)
