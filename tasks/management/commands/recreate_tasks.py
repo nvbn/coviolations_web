3d77c90443c828a1b5d171eb56e6885388aee357
nvbn <nvbn.rm@gmail.com>
2013-09-18 09:08:12 +0400

    Not flush violations

--- tasks/management/commands/recreate_tasks.py
+++ tasks/management/commands/recreate_tasks.py
@@ -8,6 +8,5 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         for task in Tasks.find({}):
-            task['violations'] = []
             task_id = Tasks.save(task)
             create_task.delay(task_id)
