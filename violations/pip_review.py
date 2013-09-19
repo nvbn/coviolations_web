4d13d6c67511b5d9888c9b0e5bbff0a091b34ad5
nvbn <nvbn.rm@gmail.com>
2013-09-19 11:34:51 +0400

    Improve pip-review representation

--- violations/pip_review.py
+++ violations/pip_review.py
@@ -1,6 +1,13 @@
+from functools import partial
+from operator import is_not
+import re
 from django.template.loader import render_to_string
 from tasks.const import STATUS_SUCCESS, STATUS_FAILED
 from .base import library
+
+
+match_line = partial(re.match, r'^(.*)==(.*) is available \(you have (.*)\)$')
+not_none = partial(is_not, None)
 
 
 @library.register('pip_review')
@@ -12,12 +19,17 @@ def pip_review_violation(data):
     :returns: dict
     """
     count = len(data['raw'].split('\n')) - 1
+
+    packages = [
+        match.groups()
+        for match in map(match_line, data['raw'].split('\n')) if match
+    ]
     data['status'] = STATUS_SUCCESS if count == 0 else STATUS_FAILED
     data['preview'] = render_to_string('violations/pip_review/preview.html', {
         'count': count,
     })
     data['prepared'] = render_to_string(
-        'violations/pip_review/prepared.html', {'raw': data['raw']},
+        'violations/pip_review/prepared.html', {'packages': packages},
     )
     data['plot'] = {
         'outdated': count,
