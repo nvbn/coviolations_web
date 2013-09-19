e0ecd5b7688d83bf2a37b2e742987b2f67e8b21b
nvbn <nvbn.rm@gmail.com>
2013-09-19 15:31:11 +0400

    Add organization factory

--- projects/tests/factories.py
+++ projects/tests/factories.py
@@ -1,5 +1,6 @@
 import factory
 from django.contrib.auth.models import User
+from accounts.tests.factories import UserFactory
 from .. import models
 
 
@@ -13,3 +14,21 @@ class ProjectFactory(factory.DjangoModelFactory):
     @factory.sequence
     def owner(n):
         return User.objects.create_user('user{}'.format(n))
+
+
+class OrganizationFactory(factory.DjangoModelFactory):
+    """Organization factory"""
+    FACTORY_FOR = models.Organization
+
+    name = factory.Sequence(lambda n: 'organization {}'.format(n))
+
+    @factory.post_generation
+    def users(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for user in extracted:
+                self.users.add(user)
+        else:
+            self.users = UserFactory.create_batch(10)
