02bf1515b951611cdaa4e58d5cae40dd1d94bf18
nvbn <nvbn.rm@gmail.com>
2013-09-19 10:27:41 +0400

    Add fancy coverage representation

--- violations/coverage.py
+++ violations/coverage.py
@@ -17,13 +17,20 @@ def coverage_violation(data):
         part for part in line.split(' ')
         if len(part) > 1 and 'TOTAL' not in part
     ]
+    each_file = [
+        filter(len, line.split(' '))
+        for line in data['raw'].split('\n')[2:-3]
+    ]
     data['preview'] = render_to_string('violations/coverage/preview.html', {
         'statements': statements,
         'miss': miss,
         'cover': cover,
     })
     data['prepared'] = render_to_string('violations/coverage/prepared.html', {
-        'raw': data['raw'],
+        'statements': statements,
+        'miss': miss,
+        'cover': cover,
+        'each_file': each_file,
     })
     data['plot'] = {
         'cover': int(cover[:-1]),
