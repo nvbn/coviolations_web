a2dcb5149f711721164b36ac14cfe05c18f74748
nvbn <nvbn.rm@gmail.com>
2013-10-08 19:13:26 +0400

    Project models tests use sure

--- projects/tests/test_models.py
+++ projects/tests/test_models.py
@@ -1,3 +1,4 @@
+import sure
 from django.test import TestCase
 from django.contrib.auth.models import User
 from accounts.tests.factories import UserFactory
@@ -20,7 +21,7 @@ class ProjectManagerCase(MockGithubMixin, TestCase):
         User.github.get_user.return_value.get_repos.return_value =\
             map(self._create_repo, range(10))
         projects = models.Project.objects.get_or_create_for_user(self.user)
-        self.assertEqual(projects.count(), 10)
+        projects.count().should.be.equal(10)
 
     def test_get(self):
         """Test get without creating new"""
@@ -33,7 +34,7 @@ class ProjectManagerCase(MockGithubMixin, TestCase):
             map(self._create_repo, range(10))
         User.github.get_user.return_value.get_repos.get_orgs = []
         projects = models.Project.objects.get_or_create_for_user(self.user)
-        self.assertEqual(projects.count(), 10)
+        projects.count().should.be.equal(10)
 
     def test_get_enabled_for_user(self):
         """"Test get enabled for user"""
@@ -43,8 +44,8 @@ class ProjectManagerCase(MockGithubMixin, TestCase):
         factories.ProjectFactory.create_batch(
             30, owner=self.user, is_enabled=False,
         )
-        self.assertItemsEqual(
-            enabled, models.Project.objects.get_enabled_for_user(self.user),
+        set(enabled).should.be.equal(
+            set(models.Project.objects.get_enabled_for_user(self.user)),
         )
 
     def test_get_for_user_in_organization(self):
@@ -54,8 +55,8 @@ class ProjectManagerCase(MockGithubMixin, TestCase):
         projects = factories.ProjectFactory.create_batch(
             10, organization=organization,
         )
-        self.assertItemsEqual(
-            projects, models.Project.objects.get_for_user(self.user),
+        set(projects).should.be.equal(
+            set(models.Project.objects.get_for_user(self.user)),
         )
 
 
@@ -75,41 +76,38 @@ class ProjectModelCase(MongoFlushMixin, TestCase):
         }}, {'project': project.name, 'commit': {
             'branch': 'develop',
         }}])
-        self.assertItemsEqual(project.branches, ['master', 'develop'])
+        set(project.branches).should.be.equal({'master', 'develop'})
 
     def test_owner_can_access(self):
         """Test owner can access project"""
         project = factories.ProjectFactory()
-        self.assertTrue(project.can_access(project.owner))
+        project.can_access(project.owner).should.be.true
 
     def test_organization_member_can_access(self):
         """Test organization member can access project"""
         project = factories.ProjectFactory()
-        self.assertTrue(project.can_access(
+        project.can_access(
             project.organization.users.all()[0],
-        ))
+        ).should.be.true
 
     def test_other_user_cant_access(self):
         """Test other user can't access"""
         project = factories.ProjectFactory(is_private=True)
         user = UserFactory()
-        self.assertFalse(project.can_access(user))
+        project.can_access(user).should.be.false
 
     def test_get_allowed_users_only_owner(self):
         """Test get allowed users only owner"""
         project = factories.ProjectFactory(
             organization=None, is_private=True,
         )
-        self.assertItemsEqual(
-            project.get_allowed_users(), [project.owner],
-        )
+        set(project.get_allowed_users()).should.be.equal({project.owner})
 
     def test_get_allowed_users_with_organization(self):
         """Test get allowed users with organization"""
         project = factories.ProjectFactory(is_private=True)
-        self.assertItemsEqual(
-            project.get_allowed_users(),
-            [project.owner] + list(project.organization.users.all()),
+        set(project.get_allowed_users()).should.be.equal(
+            set([project.owner] + list(project.organization.users.all())),
         )
 
 
@@ -123,8 +121,8 @@ class OrganizationManagerCase(TestCase):
         """Test create and add user"""
         organization =\
             models.Organization.objects.get_with_user('test', self.user)
-        self.assertEqual(organization.users.count(), 1)
-        self.assertEqual(models.Organization.objects.count(), 1)
+        organization.users.count().should.be.equal(1)
+        models.Organization.objects.count().should.be.equal(1)
 
     def test_create_with_exists_user(self):
         """Test create with exists user"""
@@ -133,8 +131,8 @@ class OrganizationManagerCase(TestCase):
         )
         organization =\
             models.Organization.objects.get_with_user('test', self.user)
-        self.assertItemsEqual(organization.users.all(), [self.user])
-        self.assertEqual(models.Organization.objects.count(), 1)
+        set(organization.users.all()).should.be.equal({self.user})
+        models.Organization.objects.count().should.be.equal(1)
 
     def test_add_user_to_exists(self):
         """Test add user to exists"""
@@ -144,4 +142,4 @@ class OrganizationManagerCase(TestCase):
         user = UserFactory()
         organization =\
             models.Organization.objects.get_with_user('test', user)
-        self.assertItemsEqual(organization.users.all(), [self.user, user])
+        set(organization.users.all()).should.be.equal({self.user, user})
