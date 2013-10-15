fdad008e60e55b50bcffa24caa23015da0d492e5
nvbn <nvbn.rm@gmail.com>
2013-10-16 02:57:39 +0400

    Attach day time statistic to resource

--- projects/resources.py
+++ projects/resources.py
@@ -67,6 +67,9 @@ class ProjectsResource(ModelResource):
     week_statistic = fields.DictField(
         attribute='week_statistic', blank=True, null=True, readonly=True,
     )
+    day_time_statistic = fields.DictField(
+        attribute='day_time_statistic', blank=True, null=True, readonly=True,
+    )
 
     class Meta:
         queryset = Project.objects.all()
